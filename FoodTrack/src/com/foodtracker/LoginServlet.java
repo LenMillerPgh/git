99e14ac3dd06b0a06917af2414df01e6c4b0d209
Len Miller <register@lenmiller.net>
2016-04-16 22:10:34 -0400

    Fix issue with capital L

--- FoodTrack/src/com/foodtracker/LoginServlet.java
+++ FoodTrack/src/com/foodtracker/LoginServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/login")
+@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
